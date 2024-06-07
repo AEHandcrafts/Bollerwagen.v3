51881f0faef470f255529ae4a48e89a07ca4b788
David Adamson <david.adamson2304@gmail.com>
2024-06-07 22:41:20 +0200

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 #include <Arduino.h>
+#include <FastLED.h>
+
+#define LED_TYPE APA102
+#define COLOR_ORDER BGR
+#define NUM_LEDS 300
+#define DATA_PIN 1
+#define CLOCK_PIN 2
+
+int ledCutoff = 154;
+
+CRGB leds[NUM_LEDS];
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println("Setup");
+  FastLED.addLeds<LED_TYPE, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS);
 }
 
+int t = 0;
+int color = 0xFF1100;
+
 void loop() {
+  t++;
+
+  if (t % 100 == 0) {
+    for (int i = 0; i < ledCutoff; i++)
+      leds[i] = CRGB::White;
+  } else {
+    for (int i = 0; i < ledCutoff; i++)
+      leds[i] = CRGB(color);
+  }
+
+  for (int i = ledCutoff; i < NUM_LEDS; i++)
+    leds[i] = CRGB(color);
+  FastLED.show();
+  delay(8);
 }
