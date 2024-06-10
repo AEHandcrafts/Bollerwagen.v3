74fc10adbcece1708a61f18219c52732720c4bb2
David Adamson <david.adamson2304@gmail.com>
2024-06-10 09:50:21 +0200

    reformat

--- src/matrix.h
+++ src/matrix.h
@@ -9,9 +9,7 @@ class Matrix {
     delete[] this->leds;
   }
   void fill(CRGB color) const {
-    for (uint32_t i = 0; i < this->width * this->height; i++) {
-      leds[i] = color;
-    }
+    for (uint32_t i = 0; i < this->width * this->height; i++) leds[i] = color;
   }
   void clear() const {
     this->fill(CRGB::Black);
